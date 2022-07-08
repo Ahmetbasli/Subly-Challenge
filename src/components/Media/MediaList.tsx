0464443d60c71f37cc2b21a1995ce2585fb2dd25
mihomihouk <mihomihouk@gmail.com>
2022-07-08 18:43:48 +0100

    Added a filtering function for two filters

--- src/components/Media/MediaList.tsx
+++ src/components/Media/MediaList.tsx
@@ -12,6 +12,7 @@ import {
   InputLabel,
   Select,
   MenuItem,
+  SelectChangeEvent,
 } from '@mui/material'
 import { makeStyles } from '@mui/styles'
 
@@ -41,7 +42,12 @@ const MediaList: React.FC = () => {
   const [isLoading, setIsLoading] = useState(false)
   const [mediums, setMediums] = useState<Media[]>([])
   const [languages, setLanguages] = useState<string[]>([])
+  const [filteredMediums, setFilteredMediums] = useState<Media[]>(mediums)
+  const [selectedStatus, setSelectedStatus] = useState<string>('all')
+  const [selectedLanguage, setSelectedLanguage] = useState<string>('all')
+
   const classes = useStyles()
+
   // Fetch Media data when first rendered
   useEffect(() => {
     setIsLoading(true)
@@ -69,7 +75,46 @@ const MediaList: React.FC = () => {
     )
     setLanguages(languagesWithoutOverlaps)
   }, [mediums])
-  console.log(ISO6391.getName('cz'))
+
+  // Change state of status filter when one of its is chosen
+  const handleStatusSelector = (e: SelectChangeEvent) => {
+    setSelectedStatus(e.target.value)
+  }
+  // Change state of language filter when one of its value is chosen
+  const handleLanguageSelector = (e: SelectChangeEvent) => {
+    setSelectedLanguage(e.target.value)
+  }
+  // Change displayed mediums when selectors are used and when a new medium is added
+  useEffect(() => {
+    // Filter array of mediums by chosen status
+    const firstFilteredMediums = filterMediumsByStatus(mediums)
+    // Filter array of remaining mediums by chosen language
+    const secondFilteredMediums = filterMediumsByLanguage(firstFilteredMediums)
+    setFilteredMediums(secondFilteredMediums)
+  }, [mediums, selectedStatus, selectedLanguage])
+
+  // Filter mediums according to chosen status
+  const filterMediumsByStatus = (mediums: Media[]) => {
+    if (selectedStatus === 'all') {
+      return mediums
+    } else {
+      return mediums.filter((medium) => {
+        return medium.status === selectedStatus
+      })
+    }
+  }
+  // Filter mediums according to chosen language
+  const filterMediumsByLanguage = (firstFilteredMediums: Media[]) => {
+    if (selectedLanguage === 'all') {
+      return firstFilteredMediums
+    } else {
+      return firstFilteredMediums.filter((medium) => {
+        return medium.languages.includes(selectedLanguage)
+      })
+    }
+  }
+  console.log(filteredMediums)
+
   return (
     <>
       <Box className={classes.container}>
@@ -77,20 +122,20 @@ const MediaList: React.FC = () => {
         <Box className={classes.filters}>
           <FormControl variant="standard" sx={{ width: '20%', mr: '10px' }}>
             <InputLabel>Status</InputLabel>
-            <Select>
-              <MenuItem>All</MenuItem>
-              <MenuItem>Ready</MenuItem>
-              <MenuItem>Error</MenuItem>
-              <MenuItem>Transcribing</MenuItem>
+            <Select onChange={handleStatusSelector} value={selectedStatus}>
+              <MenuItem value="all">All</MenuItem>
+              <MenuItem value="ready">Ready</MenuItem>
+              <MenuItem value="error">Error</MenuItem>
+              <MenuItem value="transcribing">Transcribing</MenuItem>
             </Select>
           </FormControl>
           <FormControl variant="standard" sx={{ width: '20%' }}>
             <InputLabel>Language</InputLabel>
-            <Select>
-              <MenuItem>All</MenuItem>
+            <Select onChange={handleLanguageSelector} value={selectedLanguage}>
+              <MenuItem value="all">All</MenuItem>
               {languages.length &&
                 languages.map((language) => (
-                  <MenuItem key={language}>
+                  <MenuItem key={language} value={language}>
                     {ISO6391.getName(language)}
                   </MenuItem>
                 ))}
@@ -105,11 +150,10 @@ const MediaList: React.FC = () => {
               <CircularProgress size="80px" />
             </Box>
           ) : (
-            mediums.length && (
+            filteredMediums.length && (
               <Grid container spacing={3}>
-                {mediums.map((medium) => (
+                {filteredMediums.map((medium) => (
                   <Grid item xs={6} md={4} lg={3} key={medium.id}>
-                    {/* <MediumCard medium={medium} /> */}
                     {(() => {
                       switch (medium.status) {
                         case 'error':
